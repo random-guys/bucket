f0dfcb40ff202bfbaff7af9d6f3a10beb208993c
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-06-20 14:01:51 +0100

    chore: Setup project to follow template of other lib projects

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/model';
+export * from './lib/schema';
+export * from './lib/repo';
--- src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './model';
-export * from './schema';
-export * from './repo';
