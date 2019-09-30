7a39dc1abcbba4361f30e260fcb46861eaec1f58
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-09-30 01:22:19 +0100

    fix: Export connect module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './base.model';
 export * from './base.repo';
 export * from './base.schema';
+export * from './connect';
 export * from './errors';
 export * from './query';
 export * from './utils.schema';
