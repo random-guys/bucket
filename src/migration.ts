c958ccc24a10bc41d41df4217b954dc36e12ea40
Arewa Olakunle <arewa.olakunle@gmail.com>
2020-06-09 23:14:27 +0100

    feat: how do we create and run migrations (#28)

---
+++ src/migration.ts
@@ -0,0 +1,68 @@
+import fs from "fs";
+import { down, up, config } from "migrate-mongo";
+import { Connection } from "mongoose";
+import path from "path";
+import format from "date-fns/format";
+
+const PREFIX_FORMAT = "yyyyMMddHHmmss";
+export const DEFAULT_MIGRATION_DIR = "src/migrations";
+export const DEFAULT_CONFIG_FILE_NAME = "migrate-mongo-config.js";
+
+/**
+ * Applies all unapplied migrations to the DB. Note that it passes the mongoose
+ * connection in place of `MongoClient` due to being unable to access
+ * that from mongoose.
+ * @param connection mongoose connection to be used for migration
+ */
+export function migrateUp(connection: Connection) {
+  // @ts-ignore wrong typing
+  return up(connection.db, connection.client);
+}
+
+/**
+ * Undo the last applied migration. Note that it passes the mongoose
+ * connection in place of `MongoClient` due to being unable to access
+ * that from mongoose.
+ * @param connection mongoose connection to be used for migration
+ */
+export function migrateDown(connection: Connection) {
+  // @ts-ignore wrong typing
+  return down(connection.db, connection.client);
+}
+
+export async function create(description: string) {
+  if (!description) {
+    throw new Error("Missing parameter: description");
+  }
+
+  // where should we store the new migration
+  await config.shouldExist();
+  const migrationConfig = await config.read();
+  const configuredDir = migrationConfig.migrationsDir?.replace("dist", "src") ?? DEFAULT_MIGRATION_DIR;
+  const migrationsDir = path.isAbsolute(configuredDir) ? configuredDir : path.join(process.cwd(), configuredDir);
+
+  try {
+    await fs.promises.access(migrationsDir);
+  } catch (err) {
+    throw new Error(`migrations directory does not exist: ${migrationsDir}`);
+  }
+
+  // construct the file name
+  const source = path.join(__dirname, "../samples/sample-migration.ts");
+  const prefix = format(new Date(), PREFIX_FORMAT);
+  const filename = `${prefix}-${description.split(" ").join("_")}.ts`;
+
+  // copy sample file to new file
+  const destination = path.join(migrationsDir, filename);
+  await fs.promises.copyFile(source, destination);
+
+  return destination;
+}
+
+export async function init() {
+  const source = path.join(__dirname, `../samples/${DEFAULT_CONFIG_FILE_NAME}`);
+  const destination = path.join(process.cwd(), DEFAULT_CONFIG_FILE_NAME);
+
+  await fs.promises.copyFile(source, destination);
+  return fs.promises.mkdir(path.join(process.cwd(), "src/migrations"));
+}
